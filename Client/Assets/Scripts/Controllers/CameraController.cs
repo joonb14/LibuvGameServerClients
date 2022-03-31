51e40a7b8c6deb61b8cfbfb5745e8ca5940015f0
JoonBeom Park <joonbeom_park@tmax.co.kr>
2022-03-31 14:10:29 +0900

    update camera position

--- Client/Assets/Scripts/Controllers/CameraController.cs
+++ Client/Assets/Scripts/Controllers/CameraController.cs
@@ -1,27 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public enum CameraMode
-    {
-        QuarterView,
-    }
-
-    public CameraMode _mode = CameraMode.QuarterView;
     public Vector3 _delta;
-    GameObject player;
+    GameObject _player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _delta = new Vector3(0, 6, -5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        try
+        {
+            _player = GameObject.Find("MyPlayer");
+            transform.position = _player.transform.position + _delta;
+            transform.LookAt(_player.transform);
+        }
+        catch (NullReferenceException ex)
+        {
+            Debug.Log($"MyPlayer not found: {ex}");
+        }
     }
 }
